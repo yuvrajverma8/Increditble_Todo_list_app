d7c73c6a3d804a6ba7d1a7b2398206a7238f1e53
yuvrajverma8 <125306873+yuvrajverma8@users.noreply.github.com>
2024-02-04 11:06:07 -0700

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,80 @@
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Pressable,
+  View,
+  Text,
+  ScrollView,
+  TextInput,
+  Button
+} from 'react-native';
+
+
+function App() {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Pressable>
+          <View style={[styles.task, styles.completed]}>
+            <Text style={styles.taskText}>Do laundry</Text>
+          </View>
+        </Pressable>
+        <Pressable>
+          <View style={[styles.task]}>
+            <Text style={styles.taskText}>Go to gym</Text>
+          </View>
+        </Pressable>
+        <Pressable>
+          <View style={[styles.task, styles.completed]}>
+            <Text style={styles.taskText}>Walk dog</Text>
+          </View>
+        </Pressable>
+      </ScrollView>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Add a new task..."
+        />
+        <Button title="Add" />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  task: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  },
+  completed: {
+    backgroundColor: '#e0e0e0',
+  },
+  taskText: {
+    fontSize: 16,
+  },
+  form: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginRight: 10,
+  },
+});
+
+export default App;
